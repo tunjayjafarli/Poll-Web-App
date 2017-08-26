5d4c14820fd37f5f630caec456ad872d513db0d8
Tunjay Jafarli <tunjay.jafarli@gmail.com>
2017-08-26 12:33:13 -0700

    add some customization to the django admin site

--- myapp/polls/models.py
+++ myapp/polls/models.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date pulbished')
+	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
 		return self.question_text
@@ -17,6 +17,9 @@ class Question(models.Model):
 	def was_published_recently(self):
 		now = timezone.now()
 		return now >= self.pub_date >= now - datetime.timedelta(days=1)
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
